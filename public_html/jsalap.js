b584eb48e9074c325cd4c117889cb58e0d35cb54
Lala <dlabanc@gmail.com>
2021-03-16 13:26:02 +0100

    első

---
+++ public_html/jsalap.js
@@ -0,0 +1,28 @@
+var tomb = [];
+
+function feltolt() {
+    var ertek = document.getElementById("adat").value;
+    for (var i = 0; i < ertek; i++) {
+        tomb[i] = Math.floor(Math.random() * 10) + 1; //1 és 10 közötti egész számok generálása
+    }
+}
+
+
+function dolgozz() {
+
+    feltolt();
+    var osszeg = 0;
+    for (var i = 0; i < tomb.length; i++) {
+        osszeg += tomb[i];
+    }
+    document.getElementById("szoveg").innerHTML += tomb + "<br>Az összeg: " + osszeg;
+
+}
+function init() {
+    console.log("Bejelentkeztem");
+    console.log(document.getElementById("adat").value);
+    document.getElementById("szamol").addEventListener("click", dolgozz);
+}
+
+window.addEventListener("load", init);
+
